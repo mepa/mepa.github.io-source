bbbe37129b468978b2826a7cbd9e16207a63dda9
Meghann Agarwal <meg@meghannpalmer.com>
2016-03-05 21:50:24 -0600

    Change theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,7 +34,7 @@ DEFAULT_PAGINATION = 5
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-THEME = './themes/pelican-bootstrap3'
+#THEME = './themes/pelican-bootstrap3'
 
 MARKUP = ('md', 'ipynb')
 
