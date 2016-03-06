d56be4f93fe9e087096494804ba189cfeb01c8f9
Meghann Agarwal <meg@meghannpalmer.com>
2016-03-05 21:36:34 -0600

    Update with theme, plugins

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,17 +19,24 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# # Blogroll
-# LINKS = (('Pelican', 'http://getpelican.com/'),
-#          ('Python.org', 'http://python.org/'),
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+          ('Python.org', 'http://python.org/'),)
 #          ('Jinja2', 'http://jinja.pocoo.org/'),
 #          ('You can modify those links in your config file', '#'),)
 
-# # Social widget
-# SOCIAL = (('You can add links in your config file', '#'),
-#           ('Another social link', '#'),)
+# Social widget
+SOCIAL = (('GitHub', 'https://github.com/mepa'),)
+#          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+THEME = './themes/pelican-bootstrap3'
+
+MARKUP = ('md', 'ipynb')
+
+PLUGIN_PATHS = ['./plugins']
+PLUGINS = ['ipynb.markup']
