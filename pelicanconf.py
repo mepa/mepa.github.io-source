8031394fa1e4fe79e8e5cbbc11b36924b77fee4d
Meghann Agarwal <meg@meghannpalmer.com>
2016-03-07 23:43:02 -0600

    Try elegant theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,7 +34,8 @@ DEFAULT_PAGINATION = 5
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-THEME = './themes/pelican-bootstrap3'
+#THEME = './themes/pelican-bootstrap3'
+THEME = './themes/pelican-elegant''
 
 #MARKUP = ('md', 'ipynb')
 
